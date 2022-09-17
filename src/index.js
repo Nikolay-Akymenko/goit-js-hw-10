88198e574ec4104acce58d6768cf2a12a57e1dd7
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-09-17 21:40:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/France')
+    .then(response => {
+        return response.json();
+    })
+    .then(country => {
+        console.log(country);
+    })
+    .catch(error => {
+        console.log(error);
+    }); 
+
+ 
