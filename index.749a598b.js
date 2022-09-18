feeca996ddea063e53e47382e14201b43f3f0551
Nikolay-Akymenko <103488690+Nikolay-Akymenko@users.noreply.github.com>
2022-09-18 20:24:01 +0000

    Deploying to gh-pages from @ Nikolay-Akymenko/goit-js-hw-10@b4d4f9454c91698068d7a20c1c20c1c683871d95 🚀

--- index.749a598b.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/France").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.749a598b.js.map
