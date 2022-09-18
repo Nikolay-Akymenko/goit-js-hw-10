b4d4f9454c91698068d7a20c1c20c1c683871d95
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-09-18 23:23:27 +0300

    Добавляет импорты

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import './css/styles.css';
+import { fetchCoutries } from './fetchCountries.js'
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v3.1/name/France')
-    .then(response => {
-        return response.json();
-    })
-    .then(country => {
-        console.log(country);
-    })
-    .catch(error => {
-        console.log(error);
-    }); 
+const countryName = document.querySelector('.search-box');
+const countryList = document.querySelector('.country-list');
+const countryCard = document.querySelector('.country-info');
+
+countryName.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
+
+function onSearchCountry(event) {
+    event.preventDefault();
+    searchCountry = event.terget.velue;
+    searchCountry = searchCountry.trim();
+    if (searchCountry !== '') {
+        fetchCoutries(searchCountry)
+            .then(renderCountryList)
+            .catch(error => {
+                renderCountryList('');
+                Notiflix.Notify.failure('Oops, there is no country with that name');
+            });
+    } else renderCountryList(searchCountry);
+};
+
+
 
  
